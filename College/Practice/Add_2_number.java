2916381979170803bd096c568fc98c4d7aaad5e0
Tushar-Bhatia-21 <113343003+Tushar-Bhatia-21@users.noreply.github.com>
2023-01-04 21:51:41 +0530

    Create Add_2_number.java

---
+++ College/Practice/Add_2_number.java
@@ -0,0 +1,10 @@
+public class Add__2_number 
+{
+    public static void main(String args[])
+    {
+        int x=5;
+        int y=6;
+        int z=x+y;
+        System.out.println("Sum is: "+z);
+    }    
+}
