b3074f7255ab13f49696852faec3129959a5adc6
Tushar-Bhatia-21 <113343003+Tushar-Bhatia-21@users.noreply.github.com>
2023-01-11 13:36:49 +0530

    Create 11.01.23.java

---
+++ College/LAB/11.01.23.java
@@ -0,0 +1,30 @@
+1. #############
+class Main
+{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Input String: ");
+    String c=sc.nextLine();
+    System.out.println("String is:"+c);
+    }
+ }
+
+
+
+
+
+2. ###############
+import java.util.*;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Input string: ");
+        String c=sc.nextLine();
+        System.out.println("String is: "+c);
+    }
+}
+
+3.#################
